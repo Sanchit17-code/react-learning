8731c04da2abc4ea8226630afd3e09e267018790
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2025-05-31 22:58:20 +0530

    trying lazy loading stuffs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {lazy, Suspense} from "react"
 import ReactDom from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -7,7 +7,10 @@ import About from "./components/About"
 import Contact from "./components/contact"
 import Error from "./components/Error"
 import RestaurantMenu from "./components/RestaurantMenu"
+// import Grocery from "./components/Grocery"
 
+
+const Grocery = lazy(()=> import("./components/Grocery"))
 
 const AppLayout = () => {
     return (
@@ -38,6 +41,10 @@ const appRouter = createBrowserRouter([
             { 
                 path: "/restaurants/:resId",
                 element: <RestaurantMenu />,
+            },
+            { 
+                path: "/grocery",
+                element: <Suspense fallback={<h1>Loading</h1>}>  <Grocery /> </Suspense>,
             },
             
         ],
---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery = (()=>{
+    return (
+        <div>
+            <h1>Our Grocery store and it containes lots if different components</h1>
+        </div>
+    )
+})
+
+export default Grocery;
